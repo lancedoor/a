5a9c25edee88df74acbc59ac590381ad3c8c471c
Lance <LanceDoor@163.com>
2015-12-10 03:58:43 +0800

    Broker

--- demo/mmo/server/Broker.h
+++ demo/mmo/server/Broker.h
@@ -1,11 +1,23 @@
 #pragma once
+#include "../net/Actor.h"
 
 class Broker : public Actor {
-	void Init();
-	void Term();
+public:
+	Broker() {
 
-
-	void OnPacket(const string &s) {
-		cout << s << endl;
 	}
-}
+	static void OnPacket(shared_ptr<Actor> actor, const string &s) {
+		auto self = dynamic_pointer_cast<Broker>(actor);
+		if (!self)
+			return;
+
+		cout << "[ActorId = " << self->actor_id_ << "]Broker::OnPacket(" << s << ")" << endl;
+	}
+	static void OnClosed(shared_ptr<Actor> actor, int32_t reason) {
+		auto self = dynamic_pointer_cast<Broker>(actor);
+		if (!self)
+			return;
+
+		cout << "[ActorId = " << self->actor_id_ << "]Broker::OnClosed(" << reason << ")" << endl;
+	}
+};
--- demo/mmo/server/Receptionist.h
+++ demo/mmo/server/Receptionist.h
@@ -1,4 +1,7 @@
 #pragma once
+#include "../net/Actor.h"
+#include "../net/ActorManager.h"
+#include "Broker.h"
 
 class Receptionist : public Actor {
 public:
@@ -11,6 +14,8 @@ public:
 			return;
 
 		cout << "[ActorId = " << self->actor_id_ << "]Receptionist::OnNewSession(" << session_id << ")" << endl;
+		int32_t broker_id = ActorManager::Get()->AddActor(make_shared<Broker>());
+		self->session_to_broker_[session_id] = broker_id;
 	}
 	static void OnSessionPacket(shared_ptr<Actor> actor, int32_t session_id, const string &s) {
 		auto self = dynamic_pointer_cast<Receptionist>(actor);
@@ -18,6 +23,9 @@ public:
 			return;
 
 		cout << "[ActorId = " << self->actor_id_ << "]Receptionist::OnSessionPacket(" << session_id << ", " << s << ")" << endl;
+		auto it = self->session_to_broker_.find(session_id);
+		if (it != self->session_to_broker_.end())
+			MessageManager::Get()->PutMessage(self->actor_id_, it->second, Broker::OnPacket, s);
 	}
 	static void OnSessionClosed(shared_ptr<Actor> actor, int32_t session_id, int32_t reason) {
 		auto self = dynamic_pointer_cast<Receptionist>(actor);
@@ -25,6 +33,12 @@ public:
 			return;
 
 		cout << "[ActorId = " << self->actor_id_ << "]Receptionist::OnSessionClosed(" << session_id << ", " << reason << ")" << endl;
+		auto it = self->session_to_broker_.find(session_id);
+		if (it != self->session_to_broker_.end()) {
+			MessageManager::Get()->PutMessage(self->actor_id_, it->second, Broker::OnClosed, reason);
+			self->session_to_broker_.erase(it);
+		}
 	}
-
+private:
+	unordered_map<int32_t, int32_t> session_to_broker_;
 };
