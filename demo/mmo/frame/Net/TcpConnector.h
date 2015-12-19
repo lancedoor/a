b71d37f7d7a0b6017aa4eec125f34ed2e4c19182
Lance <LanceDoor@163.com>
2015-12-19 17:43:38 +0800

    async_resolve

--- demo/mmo/frame/Net/TcpConnector.h
+++ demo/mmo/frame/Net/TcpConnector.h
@@ -7,17 +7,12 @@ class TcpConnector : public enable_shared_from_this<TcpConnector>
 {
 public:
   TcpConnector(boost::asio::io_service &io_service)
-  : io_service_(io_service){
+  : io_service_(io_service)
+  , resolver_(io_service) {
   }
-  void Connect()
-	{
-		tcp::resolver resolver(io_service_);
-		tcp::resolver::query query("localhost", "55555");
-		
-    //resolver.async_resolve(query, boost::bind(&TcpConnector::ResolveHandler, shared_from_this(), _1, _2));
-    auto it = resolver.resolve(query);
-    auto sock = make_shared<tcp::socket>(io_service_);
-    boost::asio::async_connect(*sock, it, boost::bind(&TcpConnector::ConnectHandler, shared_from_this(), sock, _1, _2));
+  void Connect() {
+    tcp::resolver::query query("localhost", "55555");
+    resolver_.async_resolve(query, boost::bind(&TcpConnector::ResolveHandler, shared_from_this(), _1, _2));
   }
 public:
   virtual void OnConnected(shared_ptr<tcp::socket> sock, const boost::system::error_code &ec) {}
@@ -33,5 +28,6 @@ private:
   }
 private:
   boost::asio::io_service &io_service_;
+  tcp::resolver resolver_;
 };
 
